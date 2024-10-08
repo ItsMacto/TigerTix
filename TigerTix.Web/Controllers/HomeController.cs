f9064133930d66d982d38ad6eaf4049a774cd215
ItsMacto <Mac@machowe.com>
2024-10-08 19:22:40 -0400

    Icon and some styling

--- TigerTix.Web/Controllers/HomeController.cs
+++ TigerTix.Web/Controllers/HomeController.cs
@@ -28,10 +28,10 @@ public class HomeController : Controller
     }
 
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
